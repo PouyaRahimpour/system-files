0713cd598d96c7ac22758b369a9413b501c80ccc
Pouya Rahimpour <91138983+PouyaRahimpour@users.noreply.github.com>
2023-04-13 05:05:22 +0330

    Create packer.lua

---
+++ nvim/lua/pouya/packer.lua
@@ -0,0 +1,82 @@
+
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd.packadd("packer.nvim")
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	-- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+    --colorschemes--
+    use({
+	'rose-pine/neovim',
+	as = 'rose-pine',
+	--config = function()
+	--require("rose-pine").setup()
+		--vim.cmd('colorscheme rose-pine')
+	--end
+    })
+    use({
+        'RRethy/nvim-base16',
+        as ='base16-colors',
+        --config = function ()
+            --require("base16-colors").setup()
+                --vim.cmd('colorscheme base16-onedark')
+        --end
+    })
+    use({
+        'xiyaowong/transparent.nvim',
+        as = 'transparent'
+    })
+    --    --
+
+    use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
+
+    use('nvim-treesitter/playground')
+    use('theprimeagen/harpoon')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
+
+    use {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v1.x',
+      requires = {
+          -- LSP Support
+          {'neovim/nvim-lspconfig'},
+          {'williamboman/mason.nvim'},
+          {'williamboman/mason-lspconfig.nvim'},
+
+          -- Autocompletion
+          {'hrsh7th/nvim-cmp'},
+          {'hrsh7th/cmp-buffer'},
+          {'hrsh7th/cmp-path'},
+          {'saadparwaiz1/cmp_luasnip'},
+          {'hrsh7th/cmp-nvim-lsp'},
+          {'hrsh7th/cmp-nvim-lua'},
+
+          -- Snippets
+          {'L3MON4D3/LuaSnip'},
+          {'rafamadriz/friendly-snippets'},
+      }
+    }
+
+    use("folke/zen-mode.nvim")
+    use("github/copilot.vim")
+    use("eandrju/cellular-automaton.nvim")
+    -- vimtex --
+    use "lervag/vimtex"
+
+    use({
+	"L3MON4D3/LuaSnip",
+	-- follow latest release.
+	tag = "v<CurrentMajor>.*",
+	run = "make install_jsregexp"
+    })
+
+end)
