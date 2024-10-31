91abea2631232a9f4b05834998d9b0064299c07c
Pouya Rahimpour <91138983+PouyaRahimpour@users.noreply.github.com>
2024-10-31 09:10:40 +0330

    Update colors.lua

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,17 +1,38 @@
---require('rose-pine').setup({
-    --disable_background = true
---})
+--vim.cmd("TransparentEnable")
+local colors = {
+    "gruvbox",
+    "base16-gruvbox-material-dark-medium",
+    "darcula",
+    "default",
+    "rose-pine",
+    "base16-chalk",
+    "base16-monokai",
+    "base16-gruvbox-dark-hard"
+}
 
-vim.cmd("TransparentEnable")
+local color_num = 0
 
-
-function ColorMyPencils(color)
-	color = color or "base16-onedark"
-	vim.cmd.colorscheme(color)
-
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
+local function set_color(color)
+    local stat_ok = pcall(vim.cmd, "colorscheme " .. color)
+    if not stat_ok then
+        vim.notify("Could not set colorscheme "..color.."!")
+    else
+        vim.cmd("color")
+    end
 end
 
-ColorMyPencils()
+local function next_color()
+    local color = colors[color_num+1]
+    set_color(color)
+    color_num = (color_num+1)%(#colors)
+end
+
+vim.api.nvim_create_user_command(
+    "Color",
+    function()
+        next_color()
+    end,
+    {}
+)
+next_color()
+
